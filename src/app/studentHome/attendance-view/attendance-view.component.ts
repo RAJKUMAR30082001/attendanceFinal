f2de148757b12f3ce256c92de89286f7ee38907d
RAJKUMAR30082001 <rajkumarc30082001@gmail.com>
2024-05-13 09:57:51 +0530

    bug fix

--- src/app/studentHome/attendance-view/attendance-view.component.ts
+++ src/app/studentHome/attendance-view/attendance-view.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit ,ViewChild} from '@angular/core';
+import { AfterViewInit, Component,OnInit } from '@angular/core';
 import { CheckValidityService } from 'src/app/check-validity.service';
 import { StudentCouchService } from 'src/app/student-couch.service';
 import { Chart} from 'chart.js/auto'
